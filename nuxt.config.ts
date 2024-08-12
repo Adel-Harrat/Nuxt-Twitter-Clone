7c694ad02ad31a9c9446f9ab33a756436b05bb1d
Adel-Harrat <adoulamst@gmail.com>
2024-08-12 20:43:51 +0100

    Base Ui Done

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-08-09",
+  devtools: { enabled: false },
+  modules: ["@nuxtjs/tailwindcss"],
+});
